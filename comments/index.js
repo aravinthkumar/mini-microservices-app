1ecd52429dcbfeda461b5bd87cc456551c249e10
Aravinth <aravinthkumar.panchatcharam@gmail.com>
2022-12-17 18:06:20 +0530

    first git push

---
+++ comments/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.json());
+const commentsByPostId = {};
+
+app.get("/posts/:id/comments", (req, res) => {});
+
+app.post("/posts/:id/comments", (req, res) => {
+  const commentId = Math.random();
+  const { content } = req.body;
+  const comments = commentsByPostId[req.params.id] || [];
+  comments.push({ id: commentId, content });
+  commentsByPostId[req.params.id] = comments;
+  res.status(201).send(commentsByPostId);
+});
+
+app.listen(4001, () => {
+  console.log("Listening in port 4001");
+});
---
+++ posts/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const posts = { };
+
+app.use(bodyParser.json());
+
+app.get('/posts',(req,res) => {
+    res.send(posts)
+})
+
+app.post('/posts',(req,res) => {
+    const { postName } = req.body;
+    postId = Math.random()
+    posts[postId] = { postId, postName};
+   res.status(201).send(posts[postId])
+})
+
+app.listen(4000, () => {
+    console.log("Listening in port 4000")
+})
